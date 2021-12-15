64d75e56b016fc4e73ad72d7de60e6e027c3a7f3
Emily Sira <esiranjo@gmail.com>
2021-12-15 21:45:22 +0300

    More styling on the form

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var centuryOfBirth =document.getElementsByName('birth century');
+var yearOfBirth =document.getElementsByName('year of birth');
+var monthOfBirth =document.getElementsByName('month of birth');
+var dateOfBirth = document.getElementById('date of birth');
