a8221505ebc82ee6b95e3657a74ffcd8a0105196
Emily Sira <esiranjo@gmail.com>
2021-12-15 22:38:01 +0300

    applying javascript if function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var centuryOfBirth =document.getElementsByName('birth century');
-var yearOfBirth =document.getElementsByName('year of birth');
-var monthOfBirth =document.getElementsByName('month of birth');
-var dateOfBirth = document.getElementById('date of birth');
+function formValidation()
+{
+var centuryOfBirth = document.myForm.birthCentury;
+var yearOfBirth = document.myForm.yearOfBirth;
+var monthOfBirth = document.myForm.monthOfBirth;
+var dateOfBirth = document.myForm.address;
+var fgender = document.myForm.fgender;
+var mgender = document.myForm.mgender; if(centuryOfBirth.value.length ==0)
+}
+alert("Please input the first two digits on your of birth, eg 1978 = '19'");
+
