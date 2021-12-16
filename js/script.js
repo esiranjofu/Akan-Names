a3a124b7949789a21638fd3eb255acb3b0cd43d4
Emily Sira <esiranjo@gmail.com>
2021-12-16 22:13:28 +0300

    introducing the radio function on dates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-function formValidation()
-{
-var centuryOfBirth = document.myForm.birthCentury;
-var yearOfBirth = document.myForm.yearOfBirth;
-var monthOfBirth = document.myForm.monthOfBirth;
-var dateOfBirth = document.myForm.address;
-var fgender = document.myForm.fgender;
-var mgender = document.myForm.mgender; if(centuryOfBirth.value.length ==0)
-}
-alert("Please input the first two digits on your of birth, eg 1978 = '19'");
-
+var dob = $("#born").val();
+var gender = $("input:radio[name=gender]:checked").val();
