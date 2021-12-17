c25a6c90baefc939419c804e01a0554581702e58
Emily Sira <esiranjo@gmail.com>
2021-12-17 20:01:05 +0300

    checking whether the date of birth is correctly captured between 1 and 31

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 function validate() {
     var genders = document.getElementsByName("gender");
-    if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1800) {
-       alert( "Please provide a valid year of birth! eg 2019" );
+    if(document.myForm.name.value == ""){
+        alert( "Kindly enter your First Name" );
+    }
+    else if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1800) {
+       alert( "Please provide a valid year of birth eg 2019" );
        document.myForm.year.focus() ;
        return false;}
-    }
     else if(document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
     document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
        alert( "Please Enter your month of birth choosing a value between 1 and 12" );
        document.myForm.month.focus() ;
        return false;}
-       else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
-       document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
-          alert( "Please provide a valid date that you were born in!" );
-          document.myForm.day.focus() ;
+       else if( document.myForm.born.value == "" || isNaN( document.myForm.born.value ) || 
+       ( document.myForm.born.value < 31 || document.myForm.born.value >= 0) ){
+          alert( "Please provide a valid date of birth choosing between 1 - 31" );
+          document.myForm.born.focus() ;
           return false;
        }
        else if(genders[0].checked==false && genders[1].checked==false ) {
-           alert("You must select male or female");
+           alert("Please choose your gender");
            return false;
        }   
        else{
-         return true ;
+         return true;}
        }
