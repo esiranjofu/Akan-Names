aa54a52ce96753e8457951dff977174a05e2e145
Emily Sira <esiranjo@gmail.com>
2021-12-18 09:28:20 +0300

    Introduced variables renaming my HTML labels

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 function validate() {
+    var yearOfBirth = document.myForm.year;
+    var monthOfBirth = document.myForm.month;
+    var dateOfBirth = document.myForm.born;
     var mgender = document.myForm.mgender;
     var fgender = document.myForm.fgender;
     if(document.myForm.name.value == ""){
         alert( "Kindly enter your First Name" );
     }
-    else if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1800) {
-       alert( "Please provide a valid year of birth eg 2019" );
+    else if( yearOfBirth.value == "" || yearOfBirth.value.length !=4 || yearOfBirth.value >2100 || yearOfBirth.value <=1800) {
+       alert( "Please provide a valid year of birth eg 2021" );
        document.myForm.year.focus() ;
        return false;}
-    else if(document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
-    document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
-       alert( "Please Enter your month of birth choosing a value between 1 and 12" );
+    else if((isNaN(monthOfBirth.value) || monthOfBirth.value <= 0 || monthOfBirth.value > 12) ||monthOfBirth.value == "" || monthOfBirth.value.length !=2 ){
+       alert( "Please Enter your month of birth eg jan =01, Feb = 02, and Dec = 12" );
        document.myForm.month.focus() ;
        return false;}
-       else if( document.myForm.born.value == "" || isNaN( document.myForm.born.value ) || 
-       ( document.myForm.born.value > 31 || document.myForm.born.value <= 0) ){
-          alert( "Please provide a valid date of birth choosing between 1 - 31" );
+       else if( dateOfBirth.value == "" || isNaN( dateOfBirth.value ) || dateOfBirth.value.length !=2 || ( dateOfBirth.value <= 0 || dateOfBirth.value > 31) ){
+          alert( "Please provide a valid date of birth eg 01 for first or 05 for fifth or  31" );
           document.myForm.born.focus() ;
           return false;
        }
        else if(mgender.checked==false && fgender.checked==false ) {
-           alert("Please choose your gender");
+           alert("Please choose your gender either Male or Female");
            return false;
        }   
        else{
