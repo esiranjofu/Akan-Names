aacc2a31d7de15cff252392bc7888a87bc791dbe
Emily Sira <esiranjo@gmail.com>
2021-12-18 09:46:55 +0300

    Introducing the function to calculate the day of birth

--- js/script.js
+++ js/script.js
@@ -27,3 +27,7 @@ function validate() {
        else{
          return true;}
        }
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var CC, YY, MM, DD, dayBorn;
