c9043e6e6a185db7a5c0eeac7172e9fad77a42a4
Emily Sira <esiranjo@gmail.com>
2021-12-19 21:37:11 +0300

    using the prompt funtion too.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var CC, DD, MM, d, dayBorn;
 
 function validate() {
     var yearOfBirth = document.myForm.year;
@@ -31,15 +32,49 @@ function validate() {
        else{
          return true;}
        }
-
-function calculateDayBorn(){
-   var year = document.myForm.year;
-   var CC = parseInt(year.substring(0,2));
-   var YY = parseInt(year.substring(2,4));
-   var MM = parseInt(document.myForm.month.value);
-   var DD = parseInt(document.myForm.born.value);
-   var d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-   console.log(d);
-   return (Math.floor(d));
-}
-console.log(calculateDayBorn);
+       var year = prompt('Year of birth again just as for the form');
+       var CC = parseInt(year.substring(0,2));
+       var YY = parseInt(year.substring(2,4));
+       var month = prompt ('Your birth month again please in the same format as for the form');
+       var MM = parseInt(month);
+       var date = prompt('And Your birthdate again please')
+       var DD = parseInt(date);
+       var d = (((CC/4-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD))%7
+       var finalAns=d.toFixed(0);
+// function getGender(){
+//     var mgender = document.myForm.mgender;
+//     var fgender = document.myForm.fgender;
+//     if(mgender.checked == true){
+//       var gender = "male";
+//     }
+//     else if(fgender.checked == true){
+//       var gender = "female";
+//     }
+//     else{
+//       return false;
+//     }
+//     switch(gender){
+//       case "male":
+//         if (dayBorn == 1){
+//           alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
+//         }
+//         else if(dayBorn == 2){
+//           alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
+//         }
+//         else if(dayBorn == 3){
+//           alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
+//         }
+//         else if(dayBorn == 4){
+//           alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
+//         }
+//         else if(dayBorn == 5){
+//           alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
+//         }
+//         else if(dayBorn == 6){
+//           alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
+//         }
+//         else if(dayValue == -0){
+//           alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
+//         } 
+//     }
+// }
