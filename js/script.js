598590058604c939f4b495747bd3f756165235ab
Emily Sira <esiranjo@gmail.com>
2021-12-20 12:32:45 +0300

    trying to figure out how to use the input values in from the form in my function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-var CC, DD, MM, d, dayBorn;
-
 function validate() {
     var yearOfBirth = document.myForm.year;
     var monthOfBirth = document.myForm.month;
@@ -36,49 +31,49 @@ function validate() {
 var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var months =['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-var name =prompt('Kindly enter your first name');
-var year = prompt('Please enter your year of birth!');
-var CC = parseInt(year.substring(0,2));
-var YY = parseInt(year.substring(2,4));
-var month = prompt ('Please enter your birth month in numbers, 1 being for January');
-var MM = parseInt(month);
-var date = prompt('And Your date or birth in numbers')
-var DD = parseInt(date);
-var d = (((CC/4-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD))%7
-var finalAns=d.toFixed(0);
-var gender = prompt('Enter your gender, either Male or Female');
-switch (gender){
-  case "Male":
-  if(finalAns==0){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[0], 'And Your Akan Name is', maleNames[0] );
-  }else if (finalAns==1){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', maleNames[1] );
-  }else if (finalAns==2){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[2], 'And Your Akan Name is', maleNames[2] );
-  }else if (finalAns==3){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[3], 'And Your Akan Name is', maleNames[3] );
-  }else if (finalAns==4){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[4], 'And Your Akan Name is', maleNames[4]);
-  }else if (finalAns==5){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', maleNames[5] );
-  }else if (finalAns==6){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[6], 'And Your Akan Name is', maleNames[6] );
-  }
-    break;
-  case "Female":
-  if(finalAns==0){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[0], 'And Your Akan Name is', femaleNames[0] );
-  }else if (finalAns==1){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', femaleNames[1] );
-  }else if (finalAns==2){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[2], 'And Your Akan Name is', femaleNames[2] );
-  }else if (finalAns==3){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[3], 'And Your Akan Name is', femaleNames[3] );
-  }else if (finalAns==4){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[4], 'And Your Akan Name is', femaleNames[4]);
-  }else if (finalAns==5){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', femaleNames[5] );
-  }else if (finalAns==6){
-    alert ('Hi '+ name, 'you were born on ' + dayOfWeek[6], 'And Your Akan Name is', femaleNames[6] );
-  }
-}
+var name = prompt('Kindly enter your first name');
+// var year = prompt('Please enter your year of birth!');
+// var CC = parseInt(year.substring(0,2));
+// var YY = parseInt(year.substring(2,4));
+// var month = prompt ('Please enter your birth month in numbers, 1 being for January');
+// var MM = parseInt(month);
+// var date = prompt('And Your date or birth in numbers')
+// var DD = parseInt(date);
+// var d = (((CC/4-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD))%7
+// var finalAns=d.toFixed(0);
+// var gender = prompt('Enter your gender, either Male or Female');
+// switch (gender){
+//   case "Male":
+//   if(finalAns==0){
+//     alert ('Hi '+ name, + 'you were born on ' + dayOfWeek[0], + 'And Your Akan Name is', maleNames[0] );
+//   }else if (finalAns==1){
+//     alert ('Hi '+ name, + 'you were born on ' + dayOfWeek[1], +'And Your Akan Name is', maleNames[1] );
+//   }else if (finalAns==2){
+//     alert ('Hi ', name, + 'you were born on ', dayOfWeek[2], + 'And Your Akan Name is', maleNames[2] );
+//   }else if (finalAns==3){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[3], 'And Your Akan Name is', maleNames[3] );
+//   }else if (finalAns==4){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[4], 'And Your Akan Name is', maleNames[4]);
+//   }else if (finalAns==5){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', maleNames[5] );
+//   }else if (finalAns==6){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[6], 'And Your Akan Name is', maleNames[6] );
+//   }
+//     break;
+//   case "Female":
+//   if(finalAns==0){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[0], 'And Your Akan Name is', femaleNames[0] );
+//   }else if (finalAns==1){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', femaleNames[1] );
+//   }else if (finalAns==2){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[2], 'And Your Akan Name is', femaleNames[2] );
+//   }else if (finalAns==3){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[3], 'And Your Akan Name is', femaleNames[3] );
+//   }else if (finalAns==4){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[4], 'And Your Akan Name is', femaleNames[4]);
+//   }else if (finalAns==5){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[1], 'And Your Akan Name is', femaleNames[5] );
+//   }else if (finalAns==6){
+//     alert ('Hi '+ name, 'you were born on ' + dayOfWeek[6], 'And Your Akan Name is', femaleNames[6] );
+//   }
+// }
